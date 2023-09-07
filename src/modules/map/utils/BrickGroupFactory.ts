07412ed6bee56a35ef759c873c058f46d84289af
witcat <witcat@nine00.com>
2023-09-07 23:39:06 +0800

    feat: 砖块合并

--- src/modules/map/utils/BrickGroupFactory.ts
+++ src/modules/map/utils/BrickGroupFactory.ts
@@ -1,10 +1,17 @@
-import { add, lte, reduceWhile, times } from "ramda";
+import { add, lte, reduce, reduceWhile, times } from "ramda";
 import { generateBrick } from "./BrickFactory";
 import { MAX_COL } from "../../../constants";
 import { Brick } from "./types";
+import { mapIndexed } from "ramda-adjunct";
 
+/**
+ * 生成若干个砖块组成的数组，但不会超过10列
+ * @param h 砖块行的宽度
+ * @returns
+ */
 export const generateBrickRow = (h: number) => {
   const row = times(generateBrick, h);
+  // 生成的砖块行的宽度不能超过10列
   return reduceWhile<Brick, [Brick[], number]>(
     (acc, cur) => lte(add(acc[1], cur[0].length), MAX_COL),
     (acc, cur) => [[...acc[0], cur], add(acc[1], cur[0].length)],
@@ -12,6 +19,34 @@ export const generateBrickRow = (h: number) => {
     row
   )[0];
 };
+
+/**
+ * 把若干个砖块合并成一个砖块
+ * @param row 砖块数组
+ * @returns
+ */
+export const mergeRow = (row: Brick[]) => {
+  const brickRows = Math.max(...row.map((b) => b.length));
+  const filled = row.map((brick) =>
+    brick.length < brickRows
+      ? [
+          ...brick,
+          ...Array.from(Array(brickRows - brick.length), () =>
+            new Array(brick[0].length).fill(null)
+          ),
+        ]
+      : brick
+  );
+  return mapIndexed((_, i) =>
+    reduce<Brick[], Brick>((acc, cur) => [...acc, ...cur[i]], [], filled)
+  )(filled[0]);
+};
+
+// export const shrinkRows = (base: Brick[], target: Brick[]) => {};
+
+// export const generateRandomGroup = (v: number) => {
+//   const rows = times(generateBrickRow, v);
+// };
 
 // export const generateBrickGroup = (
 //   /**
--- src/modules/map/utils/utils.test.ts
+++ src/modules/map/utils/utils.test.ts
@@ -1,35 +1,144 @@
 import { generateBrick } from "./BrickFactory";
-import { expect, test } from "vitest";
-import { generateBrickRow } from "./BrickGroupFactory";
+import { describe, expect, test } from "vitest";
+import { generateBrickRow, mergeRow } from "./BrickGroupFactory";
+import { Brick } from "./types";
 
-test("返回一个格式正确的砖块数据", () => {
-  const brick = generateBrick(1);
-  // 应该是一个数组
-  expect(brick).toBeInstanceOf(Array);
-  // 所有成员是数组
-  expect(brick.every((row) => Array.isArray(row))).toBe(true);
-  // 所有成员是null或者对象
-  expect(
-    brick.every((row) =>
-      row.every((item) => item === null || typeof item === "object")
-    )
-  ).toBe(true);
-  // 所有成员的长度相等
-  expect(brick.every((row) => row.length === brick[0].length)).toBe(true);
-});
-
-test("生成的砖块行的宽度不能超过10列", () => {
-  for (let i = 0; i < 6; i++) {
-    const brickRow = generateBrickRow(i);
-    // brickRow中所有砖块第一行长度相加不能超过10
+describe("砖块生成", () => {
+  test("返回一个格式正确的砖块数据", () => {
+    const brick = generateBrick(1);
+    // 应该是一个数组
+    expect(brick).toBeInstanceOf(Array);
+    // 所有成员是数组
+    expect(brick.every((row) => Array.isArray(row))).toBe(true);
+    // 所有成员是null或者对象
     expect(
-      brickRow.reduce((acc, cur) => acc + cur[0].length, 0)
-    ).toBeLessThanOrEqual(10);
-  }
+      brick.every((row) =>
+        row.every((item) => item === null || typeof item === "object")
+      )
+    ).toBe(true);
+    // 所有成员的长度相等
+    expect(brick.every((row) => row.length === brick[0].length)).toBe(true);
+  });
 });
 
-test("砖块行中砖块个数应该与给出的参数相同", () => {
-  // 与给出的参数相同
-  expect(generateBrickRow(0).length).toBe(0);
-  expect(generateBrickRow(1).length).toBe(1);
+describe("砖块行生成函数", () => {
+  test("生成的砖块行的宽度不能超过10列", () => {
+    for (let i = 0; i < 6; i++) {
+      const brickRow = generateBrickRow(i);
+      // brickRow中所有砖块第一行长度相加不能超过10
+      expect(
+        brickRow.reduce((acc, cur) => acc + cur[0].length, 0)
+      ).toBeLessThanOrEqual(10);
+    }
+  });
+
+  test("砖块行中砖块个数应该与给出的参数相同", () => {
+    // 与给出的参数相同
+    expect(generateBrickRow(0).length).toBe(0);
+    expect(generateBrickRow(1).length).toBe(1);
+  });
+});
+
+describe("砖块行生成函数", () => {
+  test("一个砖块行应该能压缩（合并）成一个格式正确的砖块", () => {
+    const a: Brick[] = [
+      [
+        [{ index: 1 }, { index: 1 }],
+        [{ index: 1 }, null],
+      ],
+      [
+        [{ index: 2 }, { index: 2 }, { index: 2 }],
+        [{ index: 2 }, null, { index: 2 }],
+      ],
+      [[{ index: 3 }, { index: 3 }, { index: 3 }]],
+    ];
+    const _a: Brick = [
+      [
+        { index: 1 },
+        { index: 1 },
+        { index: 2 },
+        { index: 2 },
+        { index: 2 },
+        { index: 3 },
+        { index: 3 },
+        { index: 3 },
+      ],
+      [{ index: 1 }, null, { index: 2 }, null, { index: 2 }, null, null, null],
+    ];
+    const b: Brick[] = [
+      [
+        [{ index: 0 }, { index: 0 }],
+        [null, { index: 0 }],
+      ],
+      [
+        [{ index: 1 }, { index: 1 }],
+        [{ index: 1 }, null],
+      ],
+      [
+        [{ index: 2 }, { index: 2 }],
+        [{ index: 2 }, null],
+        [{ index: 2 }, null],
+        [{ index: 2 }, null],
+      ],
+      [
+        [{ index: 3 }, { index: 3 }, { index: 3 }, { index: 3 }],
+        [null, { index: 3 }, null, null],
+      ],
+    ];
+    const _b: Brick = [
+      [
+        { index: 0 },
+        { index: 0 },
+        { index: 1 },
+        { index: 1 },
+        { index: 2 },
+        { index: 2 },
+        { index: 3 },
+        { index: 3 },
+        { index: 3 },
+        { index: 3 },
+      ],
+      [
+        null,
+        { index: 0 },
+        { index: 1 },
+        null,
+        { index: 2 },
+        null,
+        null,
+        { index: 3 },
+        null,
+        null,
+      ],
+      [null, null, null, null, { index: 2 }, null, null, null, null, null],
+      [null, null, null, null, { index: 2 }, null, null, null, null, null],
+    ];
+    expect(mergeRow(a)).toEqual(_a);
+    expect(mergeRow(b)).toEqual(_b);
+  });
+
+  test("合并后的砖块行，每一行的长度应该相等", () => {
+    for (let i = 0; i < 10; i++) {
+      const brickRow = generateBrickRow(5);
+      const mergedBrick = mergeRow(brickRow);
+      if (!mergedBrick.every((row) => row.length === mergedBrick[0].length)) {
+        console.log("att", mergedBrick);
+      }
+      expect(
+        mergedBrick.every((row) => row.length === mergedBrick[0].length)
+      ).toBe(true);
+    }
+  });
+
+  test("合并后的砖块行行数应该等于原来砖块行中最高的砖块行数", () => {
+    for (let i = 0; i < 10; i++) {
+      const brickRow = generateBrickRow(5);
+      const maxRow = Math.max(...brickRow.map((brick) => brick.length));
+      const mergedBrick = mergeRow(brickRow);
+
+      if (mergedBrick.length !== maxRow)
+        console.log(mergedBrick, JSON.stringify(brickRow));
+      expect(mergedBrick.length).toBe(maxRow);
+    }
+  });
 });
