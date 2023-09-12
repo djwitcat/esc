eb2b947151965f7f711e45035d6a2d1f99c6df38
witcat <witcat@nine00.com>
2023-09-12 20:10:00 +0800

    chore: breaking changes

--- src/constants.ts
+++
@@ -1,1 +0,0 @@
-export const MAX_COL = 10;
--- src/modules/map/utils/types.ts
+++
@@ -1,9 +0,0 @@
-export type Payload = {
-  index: number;
-};
-export type Pattern = (0 | 1)[][];
-export type Brick = (Payload | null)[][];
-export type BrickGroup = {
-  index: number;
-  map: Brick;
-};
