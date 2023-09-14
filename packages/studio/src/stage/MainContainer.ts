df48c55bfa87d9cdf3fe6f3287787bbf1b30b029
witcat <witcat@nine00.com>
2023-09-15 01:21:27 +0800

    feat: 清洁

---
+++ packages/studio/src/stage/MainContainer.ts
@@ -0,0 +1,29 @@
+import { Container } from "pixi.js";
+import { CANVAS_HEIGHT, CANVAS_WIDTH, CELL_SIZE } from "../constants";
+import { store } from "../store";
+
+export class MainContainer extends Container {
+  constructor() {
+    super();
+    this.init();
+  }
+
+  init() {
+    this.pivot.set(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+    this.position.set(window.innerWidth / 2, window.innerHeight / 2);
+    this.width = CANVAS_WIDTH;
+    this.height = CANVAS_HEIGHT;
+
+    this.on("click", (e) => {
+      const localPoint = this.toLocal(e.global);
+      const x = Math.floor(localPoint.x / CELL_SIZE);
+      const y = Math.floor(localPoint.y / CELL_SIZE);
+      this.setSelectedCell(x, y);
+    });
+  }
+
+  setSelectedCell(x: number, y: number) {
+    if (store.getState().editor.handMode) return;
+    store.setState({ focus: { x, y } });
+  }
+}
--- packages/studio/src/stage/initStage.ts
+++ packages/studio/src/stage/initStage.ts
@@ -1,8 +1,8 @@
 import * as PIXI from "pixi.js";
-import { CANVAS_HEIGHT, CANVAS_WIDTH, CELL_SIZE } from "../constants";
 import { CellHighlight } from "./CellHighlight";
 import { Viewport } from "./Viewport";
-import { store } from "../store";
+import { MainContainer } from "./MainContainer";
+import { createGrids } from "./nonInteractElements";
 
 export const initStage = () => {
   const app = new PIXI.Application<HTMLCanvasElement>({
@@ -10,17 +10,11 @@ export const initStage = () => {
     resizeTo: window,
   });
 
-  const { setState, getState } = store;
-
-  const container = createMainContainer((v) => {
-    //独立一个container class吧，handMove时设置eventmode为none
-    if (getState().editor.handMode) return;
-    setState({ focus: v });
-  });
-
+  const container = new MainContainer();
   const viewport = new Viewport(app);
   const cellHighlight = new CellHighlight();
 
+  container.addChild(createGrids());
   container.addChild(cellHighlight);
   viewport.addChild(container);
   app.stage.addChild(viewport);
@@ -31,46 +25,4 @@ export const initStage = () => {
     viewport.destroy();
     document.body.removeChild(app.view);
   };
-};
-
-const createMainContainer = (
-  setSelectedCell: (cell: { x: number; y: number }) => void
-) => {
-  const container = new PIXI.Container();
-  container.pivot.set(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
-  container.position.set(window.innerWidth / 2, window.innerHeight / 2);
-  container.width = CANVAS_WIDTH;
-  container.height = CANVAS_HEIGHT;
-
-  container.on("click", (e) => {
-    const localPoint = container.toLocal(e.global);
-    const x = Math.floor(localPoint.x / CELL_SIZE);
-    const y = Math.floor(localPoint.y / CELL_SIZE);
-    setSelectedCell({ x, y });
-  });
-  container.eventMode = "dynamic";
-
-  const grids = createGrids();
-  container.addChild(grids);
-
-  return container;
-};
-
-const createGrids = () => {
-  const grids = new PIXI.Graphics();
-  grids.beginFill(0x000000);
-  grids.drawRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  grids.endFill();
-  for (let i = 0; i <= 10; i++) {
-    grids.lineStyle(4, 0xffffff);
-    grids.moveTo(CELL_SIZE * i, 0);
-    grids.lineTo(CELL_SIZE * i, CANVAS_HEIGHT);
-  }
-
-  for (let i = 0; i <= Math.floor(CANVAS_HEIGHT / CELL_SIZE); i++) {
-    grids.lineStyle(4, 0xffffff);
-    grids.moveTo(0, CELL_SIZE * i);
-    grids.lineTo(CANVAS_WIDTH, CELL_SIZE * i);
-  }
-  return grids;
 };
